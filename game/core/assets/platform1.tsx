615ec19fd536db724b21ad1f95e174047214653d
Shehryar21 <37384882+Shehryar21@users.noreply.github.com>
2019-06-13 01:26:16 -0400

    Merge branch 'master' of https://github.com/kumailraza1/ICS4U-FSE
    # Conflicts:
    #	game/core/src/com/mygdx/game/GameClass.java

---
+++ game/core/assets/platform1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="platform1" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="6" columns="3">
+ <image source="Tiles/13.png" width="128" height="93"/>
+</tileset>
